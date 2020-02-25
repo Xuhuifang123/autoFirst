178eef6e94af5d757bb57030b76a6dbc7802c267
好久不见 <haojiubujian@haojiubujiandeMacBook-Air.local>
2020-02-25 11:59:07 +0800

    update

--- src/api/sharephoto.js
+++ src/api/sharephoto.js
@@ -1,9 +1,12 @@
+import wepy from 'wepy'
+
+let Width,
+    Height,
+    ctx;
+
 //参数依次为  二维码图片，
-const share =  (  code  ) => {
-    // var that = this;
-    let Width,
-        Height;
-    
+const Share =  (  code,storyImage,storyCanvas,storyTitle  ) => {
+
     //获取系统信息，具体的属性请前往微信小程序官网查看
     wx.getSystemInfo({
         success: res => {
@@ -14,20 +17,18 @@ const share =  (  code  ) => {
     })
     
     var qrcode = code//二维码图片一般为网络图片后台生成
-    var bgImgPath = that.storyImage //首先你需要准备一张背景图
-    var bgborder = './../res/images/border_icon.png'
-    var width = that.Width
-    var height = that.Height
-    that.ctx = wx.createCanvasContext('storyCanvas')//创建 canvas 绘图上下文
+    var bgImgPath = storyImage //首先你需要准备一张背景图
+    var bgborder = './../../res/images/border_icon.png'
+    ctx = wx.createCanvasContext(storyCanvas)//创建 canvas 绘图上下文
     
     //画矩形 设置背景颜色
-    that.ctx.rect(0,0,282,365);
-    that.ctx.fillStyle = "#fff";
-    that.ctx.fill();
+    ctx.rect(0,0,282,365);
+    ctx.fillStyle = "#fff";
+    ctx.fill();
     
     //将文章图绘制到画布中
-    that.ctx.drawImage(bgImgPath, 0, 0, 282, 188)
-    that.ctx.save() 
+    ctx.drawImage(bgImgPath, 0, 0, 282, 188)
+    ctx.save() 
 
     //下面你需要描述的文字，因为canvas文字不能够换行，所以这里我们按行一行一行写，当然你也可以自己写一个函数将文字截成一段一段的循环放入画布中
     //标题
@@ -41,69 +42,54 @@ const share =  (  code  ) => {
         size: 15,
         align: 'left',
         baseline: 'top',
-        text: that.storyInfo.title,
+        text: storyTitle,
         bold: true
     };
-    that.textWrap(title);
+    textWrap(title);
 
     // 标题下直线
-    that.ctx.drawImage(bgborder, 12, 248, 259, 1);
-    that.ctx.save();
+    ctx.drawImage(bgborder, 12, 248, 259, 1);
+    ctx.save();
     
     // 绘制二维码
-    that.ctx.drawImage(qrcode, 24, 258, 98, 98);
-    that.ctx.save();
+    ctx.drawImage(qrcode, 24, 258, 98, 98);
+    ctx.save();
 
     // 提示阅读
-    that.ctx.setFillStyle('#000000');
-    that.ctx.setFontSize(12);
-    that.ctx.setTextAlign('left');
-    that.ctx.setTextBaseline('top');
-    that.ctx.fillText( '长按图片识别小程序码', 138, 294);
-    that.ctx.fillText( '进入汽势传媒阅读全文', 138, 310);
-    that.ctx.draw()
-
+    ctx.setFillStyle('#000000');
+    ctx.setFontSize(12);
+    ctx.setTextAlign('left');
+    ctx.setTextBaseline('top');
+    ctx.fillText( '长按图片识别小程序码', 138, 294);
+    ctx.fillText( '进入汽势传媒阅读全文', 138, 310);
+    ctx.draw()
+    let CanvasRes = '';
     //将canvas画布转化为图片
-    wx.canvasToTempFilePath({
-        x: 0,
-        y: 0,
-        width: that.Width,
-        height: that.Height,
-        destWidth: that.Width,
-        destHeight: that.Height,
-        canvasId: 'storyCanvas',
-        success: function (res) {
-            console.log('imagePath'+res.tempFilePath);
-            /* 这里 就可以显示之前写的 预览区域了 把生成的图片url给image的src */
-            that.imagePath = res.tempFilePath;
-            that.$apply();
-        },
-        fail: function (res) {
-            console.log(res)
-        }
-    })
-}
-
-// 保存文章题图到本地
-const saveImageLocal = () => {
-    let that = this;
-    wx.downloadFile({
-        url: that.storyInfo.title_pic1,
-        success: function(res) {
-            if (res.statusCode === 200){
-                let img = res.tempFilePath;
-                that.storyImage = img;
-                console.log( that.storyImage );
-                that.share();
+    return new Promise(function(resolve,reject){
+        wx.canvasToTempFilePath({
+            x: 0,
+            y: 0,
+            width: Width,
+            height: Height,
+            destWidth: Width,
+            destHeight: Height,
+            canvasId: storyCanvas,
+            success: function (res) {
+                /* 这里 就可以显示之前写的 预览区域了 把生成的图片url给image的src */
+                // CanvasRes = res.tempFilePath;
+                resolve({'value':res.tempFilePath});
+            },
+            fail: function (res) {
+                resolve({'value':'图片加载失败，刷新重试 ~'});　　
             }
-        }
-    })
+        })
+    });
+    
 }
 
 // 文本换行
-textWrap (obj) {
-    let that = this;
-    let tr = that.getTextLine(obj);
+const textWrap = (obj) => {
+    let tr = getTextLine(obj);
     for (let i = 0; i < tr.length; i++) {
         if (i < obj.line){
             let txt = {
@@ -120,21 +106,20 @@ textWrap (obj) {
                 // console.log( txt.text );
                 // txt.text = txt.text.substring(0, txt.text.length - 3) + '......';
             // }
-            that.drawText(txt);
+            drawText(txt);
         }
     }
 };
 
 // 获取文本折行
-getTextLine (obj){
-    let that = this;
-    that.ctx.setFontSize(obj.size);
+const getTextLine = (obj) => {
+    ctx.setFontSize(obj.size);
     let arrText = obj.text.split('');
     let line = '';
     let arrTr = [];
     for (let i = 0; i < arrText.length; i++) {
         var testLine = line + arrText[i];
-        var metrics = that.ctx.measureText(testLine);
+        var metrics = ctx.measureText(testLine);
         var width = metrics.width;
         if (width > obj.width && i > 0) {
             arrTr.push(line);
@@ -150,30 +135,24 @@ getTextLine (obj){
 };
 
 // 渲染文字
-drawText(obj) {
-    let that = this;
-    that.ctx.save();
-    that.ctx.setFillStyle(obj.color);
-    that.ctx.setFontSize(obj.size);
-    that.ctx.setTextAlign(obj.align);
-    that.ctx.setTextBaseline(obj.baseline);
+const drawText = (obj) => {
+    ctx.save();
+    ctx.setFillStyle(obj.color);
+    ctx.setFontSize(obj.size);
+    ctx.setTextAlign(obj.align);
+    ctx.setTextBaseline(obj.baseline);
     if (obj.bold) {
-        console.log('字体加粗')
-        that.ctx.fillText(obj.text, obj.x, obj.y - 0.5);
-        that.ctx.fillText(obj.text, obj.x - 0.5, obj.y);
+        ctx.fillText(obj.text, obj.x, obj.y - 0.5);
+        ctx.fillText(obj.text, obj.x - 0.5, obj.y);
     }
-    that.ctx.fillText(obj.text, obj.x, obj.y);
+    ctx.fillText(obj.text, obj.x, obj.y);
     if (obj.bold) {
-        that.ctx.fillText(obj.text, obj.x, obj.y + 0.5);
-        that.ctx.fillText(obj.text, obj.x + 0.5, obj.y);
+        ctx.fillText(obj.text, obj.x, obj.y + 0.5);
+        ctx.fillText(obj.text, obj.x + 0.5, obj.y);
     }
-    that.ctx.restore();
+    ctx.restore();
 };
 
-
-
-
-
 module.exports = {
-    share
+    Share
 }
